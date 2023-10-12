e5507a3706b1900bded87844f9e28f1d9e4e318e
HiroIrie <h0514c0325@gmail.com>
2023-10-13 07:09:28 +0900

    client実装

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const cors=require('cors');
 
 
 const PORT=8000;
 const app = express();
 app.use(express.json());
-
+app.use(cors({
+    origin: 'http://localhost:3000', // クライアントのオリジン
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // 許可するHTTPメソッド
+    credentials: true, // 許可するクッキーを含むリクエスト
+}))
 
 app.use('/api',require('./src/routes'));
 
--- server/src/routes/download.js
+++ server/src/routes/download.js
@@ -1,9 +1,8 @@
 const express=require('express');
 const{downloadMovie}=require('../../utils/downloadMovie');
 const router = express.Router();
-const {upload} = require('../../utils/multer');
 
 
-router.post('/', upload.single('video'),downloadMovie);
+router.post('/',downloadMovie);
 
 module.exports = router;
